a60bc3af164326c59a0e209e209682065625a1e2
Sia <siakaramalegos@gmail.com>
2015-02-23 09:18:02 -0600

    Add current time and custom greeting to page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,22 @@
 class HomeController < ApplicationController
   def index
+    right_now = Time.now
+    @current_time = right_now.to_formatted_s(:short)
+
+    # Adjust message by time of day
+    # Case statement instead of a lot of elsifs
+    @greet_message = case right_now.hour
+    when 5..11
+      "Good morning!  Kalimera!"
+    when 12..15
+      "Good afternoon! Kalimera"
+    when 16..19
+      "Good evening!  Kalispera!"
+    when 20..23, 0..4
+      "Good night.  Kalinixta."
+    end
+
   end
+
+
 end
