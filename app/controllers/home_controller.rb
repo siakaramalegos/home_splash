2e0d004623c602e6018f248f97f8e930df94a8d2
Sia <siakaramalegos@gmail.com>
2015-02-23 12:13:32 -0600

    Add multiple languages, time stamp gem, and images for future background

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,20 @@
 class HomeController < ApplicationController
   def index
     right_now = Time.now
-    @current_time = right_now.to_formatted_s(:short)
+    @current_time = right_now.stamp("12:00")
+    #@current_date = right_now.stamp("feb 1, 2015")
 
     # Adjust message by time of day
     # Case statement instead of a lot of elsifs
     @greet_message = case right_now.hour
     when 5..11
-      "Good morning!  Καλημέρα!"
+      "good morning  早安  καλημέρα  صباح الخير  chào buổi sáng  おはよう  buenos días"
     when 12..15
-      "Good afternoon! Καλημέρα"
+      "good afternoon  下午好  καλημέρα  مساء الخير  xin chào buổi chiều    こんにちは  buenas tardes"
     when 16..19
-      "Good evening!  Καλησπέρα!"
+      "good evening  晚上好  καλησπέρα  مساء الخير  xin chào buổi tối    こんばんは  buena noches"
     when 20..23, 0..4
-      "Good night.  Καληνύχτα."
+      "good night  晚安  καληνύχτα  ليلة سعيدة  đêm tốt    おやすみ  buenas noches"
     end
 
   end
