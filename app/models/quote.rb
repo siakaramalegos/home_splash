a345523e48a369478116ed2d2a7185144fe92f31
Sia <siakaramalegos@gmail.com>
2015-03-04 09:51:30 -0600

    Add quotes to home

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,4 @@
 class Quote < ActiveRecord::Base
-  # Creating a recent scope that gives items created within the last week.
-  scope :recent, -> {where("created_at > ?", 1.week.ago)}
+  # Creating a recent scope that gives items created within the last week.  The lamda will freshly run every time the scope is called.
+  scope :recent, -> (weeks_past=1) {where("created_at > ?", weeks_past.weeks.ago)}
 end
