e6678c205221b4e35f6a96b3f6b363db0408528a
Sia <siakaramalegos@gmail.com>
2015-03-04 09:17:38 -0600

    Add seeds and recent scope for quotes model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+  # Creating a recent scope that gives items created within the last week.
+  scope :recent, -> {where("created_at > ?", 1.week.ago)}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 Quote.create(quip: "Not all those who wander are lost.")
 Quote.create(quip: "Be the change you want to see in the world.")
 Quote.create(quip: "Decide that you want it more than you are afraid of it.")
+Quote.create(quip: "Those who dare to fail miserably can achieve greatly.")
+Quote.create(quip: "The greatest accomplishment is not in never falling, but in rising again after you fall.")
+Quote.create(quip: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.")
+Quote.create(quip: "I sustain myself with the love of family.")
