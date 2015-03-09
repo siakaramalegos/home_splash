f3afb48d8e6d67bf82269bed2c38b3fcee7c403c
Sia <siakaramalegos@gmail.com>
2015-03-09 09:00:55 -0500

    Add action mailer default url to test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # config/environments/{development,test}.rb
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
