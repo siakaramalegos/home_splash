837e38c44ad89765a56e5dbefca44576726171a2
Sia <siakaramalegos@gmail.com>
2015-03-10 09:44:26 -0500

    Add style to session forms and notices

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,6 @@
 class Quote < ActiveRecord::Base
+  validates :quip, presence: true, length: {minimum: 3}, uniqueness: true
+
   # Creating a recent scope that gives items created within the last week.  The lamda will freshly run every time the scope is called.
   scope :recent, -> (weeks_past=1) {where("created_at > ?", weeks_past.weeks.ago)}
 end
